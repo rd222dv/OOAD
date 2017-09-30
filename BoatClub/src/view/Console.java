1314cdbda8b1042db7ddf262005afef96eb67200
Bruthzilla <ruth.dirnfeld@gmail.com>
2017-09-30 23:57:29 +0200

    Console almost done - TODO Boathandlers and Errorhandlers

--- BoatClub/src/view/Console.java
+++ BoatClub/src/view/Console.java
@@ -13,6 +13,7 @@ public class Console {
 	Scanner sc = new Scanner(System.in);
 	private MemberCatalog members;
 	private int selection;
+	private double in;
 
 	public Console() {
 		members = new MemberCatalog();
@@ -24,10 +25,10 @@ public class Console {
 
 	// Selection options
 	public void welcomeOptions() {
-		System.out.println(
-				"\n------------------------------" + "\nSelect one of the following choices:" + "\n 1 Create a new member"
-						+ "\n 2 View a compact list of members " + "\n 3 View a verbose list of members");
-		
+		System.out.println("\n------------------------------" + "\nSelect one of the following choices:"
+				+ "\n 1 Create a new member" + "\n 2 View a compact list of members "
+				+ "\n 3 View a verbose list of members");
+
 		selection = sc.nextInt();
 
 		// Establishing choice
@@ -36,11 +37,11 @@ public class Console {
 			addMember(getNameFromInput(), getPersonnumberFromInput());
 			welcomeOptions();
 		} else if (selection == 2) {
-			System.out.println("Retriving compact list...");
+			System.out.println("Retriving compact list..." + "\n------------------------------");
 			viewCompactList();
 			memberOptions();
 		} else if (selection == 3) {
-			System.out.println("Retriving verbose list...");
+			System.out.println("Retriving verbose list..." + "\n------------------------------");
 			viewVerboseList();
 			memberOptions();
 		} else
@@ -50,16 +51,16 @@ public class Console {
 	public void memberOptions() {
 		System.out.println("\n------------------------------" + "\nSelect one of the following choices:"
 				+ "\n 1 View Member" + "\n 2 return ");
-		
+
 		selection = sc.nextInt();
 		Member member;
 
 		// Establishing choice
 		if (selection == 1) {
-			System.out.println("View Member...");
+			System.out.println("------------------------------" + "\nTo view the Member...\n");
 			member = getChosenMember();
 			showSelectedMember(member);
-			viewMember();
+			viewMember(member);
 		} else if (selection == 2) {
 			System.out.println("return...");
 			welcomeOptions();
@@ -68,36 +69,44 @@ public class Console {
 
 	}
 
-	public void viewMember() {
+	public void viewMember(Member member) {
 		System.out.println("\n------------------------------" + "\nSelect one of the following choices:"
 				+ "\n 1 Update Member" + "\n 2 Delete Member" + "\n 3 View Boat details" + "\n 4 return ");
-		
+
 		selection = sc.nextInt();
+		Boat boat;
 
 		// Establishing choice
 		if (selection == 1) {
 			System.out.println("update Member...");
-			// TODO
+			updateMember(member, getNameFromInput(), getPersonnumberFromInput());
+			memberOptions();
 		} else if (selection == 2) {
-			System.out.println("delete Member...");
-			// TODO
+			System.out.println("To delete the Member...\n");
+			member = getChosenMember();
+			removeMember(member);
+			welcomeOptions();
 		} else if (selection == 3) {
 			System.out.println("view Boat details...");
-			viewBoatDetails();
+			boat = getChosenBoat(member);
+			viewBoatDetails(member, boat);
 		} else if (selection == 4) {
 			System.out.println("return...");
-
+			memberOptions();
 		} else
 			System.err.println("Invalid choice! Try again ");
 
 	}
 
-	public void viewBoatDetails() {
+	public void viewBoatDetails(Member member, Boat boat) {
 		System.out.println("\n------------------------------" + "\nSelect one of the following choices:"
 				+ "\n 1 Add a Boat" + "\n 2 Edit Boat" + "\n 3 Remove Boat" + "\n 4 return ");
-		
+
+		selection = sc.nextInt();
+
 		if (selection == 1) {
 			System.out.println("Add Boat...");
+			addBoat(member, getTypeFromInput(), getLengthFromInput());
 			// TODO
 		} else if (selection == 2) {
 			System.out.println("Edit Boat...");
@@ -116,34 +125,31 @@ public class Console {
 	 * Methods starting
 	 */
 	/**
-	 * Member methods starting
-	 **/
-	public void addMember(String name, String personnumber) { // TODO checkers
-																// for invalid
-																// input needed
+	 * Member methods
+	 */
+	public void addMember(String name, String personnumber) { // FIXME checkers
+																// for correct
+																// input
 		try {
 			members.addMember(name, personnumber);
 			System.out.println("New member was added!!");
 		} catch (ParseException e) {
-			System.err.println("Incorrect something"); // FIXME
+			System.err.println("Incorrect something"); // FIXME Errorhandling
 		}
 	}
 
 	public void updateMember(Member member, String name, String personnumber) {
 		try {
-			members.updateMember(member, name, personnumber); // memberList.remove(currentMember);
-			System.out.println("Member Info has been updated"); // memberList.add(new
-																// Member(name,
-																// personnumber,
-																// newID));
+			members.updateMember(member, name, personnumber);
+			System.out.println("Member Info has been updated");
 		} catch (ParseException e) {
-			System.err.println("Incorrect something"); // FIXME
+			System.err.println("Incorrect something"); // FIXME Errorhandling
 		}
 	}
 
 	public void removeMember(Member member) {
 		members.removeMember(member);
-		System.out.println("Member deleted!!");
+		System.out.println("\nMember deleted!!" + "\n------------------------------");
 	}
 
 	public Member getChosenMember() {
@@ -152,11 +158,11 @@ public class Console {
 			welcomeOptions();
 		}
 		viewCompactList();
-		System.out.print("Enter Member ID to view Member Details\n>");
+		System.out.print("\n...Enter the Member's ID\n>");
 		selection = sc.nextInt();
 
-		//if wrong Input DO something FIXME
-		
+		// if wrong Input DO something FIXME
+
 		return members.searchMember(selection);
 	}
 
@@ -165,58 +171,14 @@ public class Console {
 		System.out.printf("Name: " + selectedMember.getName() + "\t Personal number: "
 				+ selectedMember.getPersonnumber() + "\t Number of Boats: " + selectedMember.getNumberOfBoats());
 		if (selectedMember.getNumberOfBoats() > 0) {
-			//int i = 0;
 			for (Boat boat : selectedMember.getBoatList())
-				System.out.println("\nBoat Type: " + boat.getType() + "\nBoat Type: " + boat.getSize());
+				System.out.println("\nBoat Type: " + boat.getType() + "\t Boat Length: " + boat.getSize());
 		}
 	}
 
 	/**
-	 * Boat methods starting
-	 **/
-
-	public void registerBoat(Member member, boatType type, double boatLength) {
-		try {
-			member.addBoat(type, boatLength);
-			System.out.println("New boat was added!!");
-		} catch (Exception e) {
-			System.err.println("Incorrect something"); // FIXME
-		}
-	}
-
-	public void updateBoat(Member member, boatType type, double boatLength, Boat boat) {
-		try {
-			member.updateBoat(boat, type, boatLength); // boatList.remove(currentBoat);
-			System.out.println("Boat Info has been updated"); // boatList.add(new
-																// Boat(type,
-																// size));
-		} catch (Exception e) {
-			System.err.println("Incorrect something"); // FIXME
-		}
-	}
-
-	public void removeBoat(Member member, Boat boat) {
-		member.removeBoat(boat);
-		System.out.println("Boat deleted!!");
-	}
-
-	/**
-	 * input Methods
-	 * 
-	 * @return
-	 **/
-
-	public String getNameFromInput() {
-		System.out.print("Name\n>");
-		String in = sc.next() + sc.nextLine();
-		return in;
-	}
-
-	public String getPersonnumberFromInput() {
-		System.out.print("Personnumber in format YYYYMMDDXXXX\n>");
-		String in = sc.next();
-		return in;
-	}
+	 * List methods
+	 */
 
 	public void viewCompactList() {
 		if (members == null)
@@ -243,4 +205,64 @@ public class Console {
 			}
 		}
 	}
+
+	/**
+	 * Boat methods
+	 */
+
+	public void addBoat(Member member, boatType type, double boatLength) {
+		try {
+			member.addBoat(type, boatLength);
+			System.out.println("New boat was added!!");
+		} catch (Exception e) {
+			System.err.println("Incorrect something"); // FIXME Errorhandling
+		}
+	}
+
+	public void updateBoat(Member member, boatType type, double boatLength, Boat boat) {
+		try {
+			member.updateBoat(boat, type, boatLength);
+			System.out.println("Boat Info has been updated");
+		} catch (Exception e) {
+			System.err.println("Incorrect something"); // FIXME Errorhandling
+		}
+	}
+
+	public void removeBoat(Member member, Boat boat) {
+		member.removeBoat(boat);
+		System.out.println("Boat deleted!!");
+	}
+
+	/**
+	 * input Methods
+	 * 
+	 * @return
+	 */
+
+	public String getNameFromInput() {
+		System.out.print("Name\n>");
+		String in = sc.next() + sc.nextLine();
+		return in;
+	}
+
+	public String getPersonnumberFromInput() {
+		System.out.print("Personnumber in format YYYYMMDDXXXX\n>");
+		String in = sc.next();
+		return in;
+	}
+
+	private Boat getChosenBoat(Member member) {
+		return null;
+	}
+
+	private boatType getTypeFromInput() {
+		return null;
+	}
+
+	private double getLengthFromInput() { //FIXME Errorhandling
+		System.out.print("Enter Length in meters\n>");
+		in = sc.nextDouble();
+		return in;
+	}
+
 }
