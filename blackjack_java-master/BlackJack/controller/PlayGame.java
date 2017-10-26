bf1d9ea8c5c4f2a370227a0b052bc3158d423f2a
u-pi <hic.3t.nunc@gmail.com>
2017-10-26 21:27:15 +0200

    Remove hidden dependancies between view and controller

--- blackjack_java-master/BlackJack/controller/PlayGame.java
+++ blackjack_java-master/BlackJack/controller/PlayGame.java
@@ -4,25 +4,33 @@ import BlackJack.view.IView;
 import BlackJack.model.Game;
 
 public class PlayGame {
+	
+	Game game;
+	IView view;
+	
+	public PlayGame(Game a_game, IView a_view) {
+		this.game = a_game;
+		this.view = a_view;
+	}
 
-	public boolean Play(Game a_game, IView a_view) {
-		a_view.DisplayWelcomeMessage();
+	public boolean Play() {
+		view.DisplayWelcomeMessage();
 
-		a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
-		a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+		view.DisplayDealerHand(game.GetDealerHand(), game.GetDealerScore());
+		view.DisplayPlayerHand(game.GetPlayerHand(), game.GetPlayerScore());
 
-		if (a_game.IsGameOver()) {
-			a_view.DisplayGameOver(a_game.IsDealerWinner());
+		if (game.IsGameOver()) {
+			view.DisplayGameOver(game.IsDealerWinner());
 		}
 
-		int input = a_view.GetInput();
+		int input = view.GetInput();
 
 		if (input == 'p') {
-			a_game.NewGame();
+			game.NewGame();
 		} else if (input == 'h') {
-			a_game.Hit();
+			game.Hit();
 		} else if (input == 's') {
-			a_game.Stand();
+			game.Stand();
 		}
 
 		return input != 'q';
