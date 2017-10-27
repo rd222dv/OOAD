182771456c64b5c9ff22c5f91cad805caee69af4
ab223zm <alexbjaremo@gmail.com>
2017-10-27 10:03:57 +0200

    Update InternationalNewGameStrategy.java

--- blackjack_java-master/BlackJack/model/rules/InternationalNewGameStrategy.java
+++ blackjack_java-master/BlackJack/model/rules/InternationalNewGameStrategy.java
@@ -7,20 +7,10 @@ import BlackJack.model.Card;
 
 class InternationalNewGameStrategy implements INewGameStrategy {
 
-	public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-		Card c;
-
-		c = a_deck.GetCard();
-		c.Show(true);
-		a_player.AddToHand(c);
-
-		c = a_deck.GetCard();
-		c.Show(true);
-		a_dealer.AddToHand(c);
-
-		c = a_deck.GetCard();
-		c.Show(true);
-		a_player.AddToHand(c);
+	public boolean NewGame(Dealer a_dealer, Player a_player) {
+		a_dealer.GiveCard(a_player);
+		a_dealer.GiveCard(a_dealer);
+		a_dealer.GiveCard(a_player);		
 
 		return true;
 	}
